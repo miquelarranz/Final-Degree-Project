4098cfae37d1f607d9225dbd0a5c8ab1a44c8c87
miquelarranz <miquelarranz@github.com>
2015-04-03 16:51:33 +0200

    Production testing

--- app/config/database.php
+++ app/config/database.php
@@ -54,7 +54,7 @@ return array(
 
 		'mysql' => array(
 			'driver'    => 'mysql',
-            'host'      => 'droplet.miquelarranz.com',
+            'host'      => '127.0.0.1',
             'database'  => 'tfg',
             'username'  => 'root',
             'password'  => 'root',
