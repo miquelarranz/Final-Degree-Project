54377982bd1eff5d771a43df9fefd9508a16aae9
miquelarranz <miquelarranz@github.com>
2015-04-03 12:30:56 +0200

    Home view almost finished. Some details have to be changed.

--- app/views/layouts/default.blade.php
+++ app/views/layouts/default.blade.php
@@ -4,9 +4,9 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Document</title>
-        <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css"/>
+        <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css"/>
         <link rel="stylesheet" href="/css/style.css"/>
-    </head>
+        <link href='http://fonts.googleapis.com/css?family=Lato:100,300,400,700,900,100italic,300italic,400italic,700italic,900italic' rel='stylesheet' type='text/css'>    </head>
     <body>
 
         @include('layouts.partials.nav')
@@ -18,6 +18,6 @@
         </div>
 
         <script src="//code.jquery.com/jquery.js"></script>
-        <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
+        <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
     </body>
 </html>
--- app/views/pages/home.blade.php
+++ app/views/pages/home.blade.php
@@ -7,8 +7,37 @@
         </div>
     </div>
     <div class="row">
-        <div class="col-md-10 col-md-offset-1">
-            <img src="/images/skyline-home.jpg" class="img-responsive"/>
+        <div class="col-md-8 col-md-offset-2">
+            <img src="/images/skyline-home.jpg" class="home-skyline img-responsive"/>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            {{ Form::open() }}
+                <div class="row">
+                    <div class = "form-group col-xs-10 col-xs-offset-1 col-sm-6 col-sm-offset-3">
+                        {{ Form::select('city', array('Barcelona', 'London'), null, ['class' => 'form-control', 'required' => 'required']) }}
+                    </div>
+                </div>
+                <div class="row">
+                    <div class = "col-xs-10 col-xs-offset-1 col-sm-6 col-sm-offset-3">
+                        <h4 class="home-title-search text-center">Start looking for events right now!</h4>
+                        <div class="input-group">
+                            {{ Form::text('keyword', null, ['class' => 'form-control', 'required' => 'required']) }}
+                            <span class="input-group-addon">
+                                <span class="glyphicon glyphicon-search" aria-hidden="true"></span>
+                            </span>
+                        </div>
+                    </div>
+                </div>
+            {{ Form::close() }}
+        </div>
+    </div>
+    <div class="row">
+        <div class = "col-xs-10 col-xs-offset-1 col-sm-8 col-sm-offset-2">
+            <p class="text-center home-users-guide-text">
+                {{ link_to_route('home', 'Do you need some help? Download the users guide here!') }}
+            </p>
         </div>
     </div>
 @endsection
