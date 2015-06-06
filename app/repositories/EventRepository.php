aadedae9775564c3a7aac6213add3d1bae4d55e2
miquelarranz <miquelarranz@github.com>
2015-06-06 14:26:03 +0200

    Some issues fixed

--- app/repositories/EventRepository.php
+++ app/repositories/EventRepository.php
@@ -7,116 +7,123 @@ class EventRepository implements RepositoryInterface {
 
     public function all(array $related = null)
     {
-        if ( ! array_key_exists('clean', $related) and ! is_null($related))
+        if ( ! is_null($related))
         {
-            $queryHeader = "SELECT DISTINCT e.* FROM events e";
-            $query = " WHERE ";
-            $first = true;
-            //, things t, things pt, places p, openDataCities o
-            $startDateAdded = false;
-            $limit = 50;
+            if ( ! array_key_exists('clean', $related))
+            {
+                $queryHeader = "SELECT DISTINCT e.* FROM events e";
+                $query = " WHERE ";
+                $first = true;
+                //, things t, things pt, places p, openDataCities o
+                $startDateAdded = false;
+                $limit = 50;
 
-            if (array_key_exists('limit', $related))
-            {
-                $limit = $related['limit'];
-            }
-            if (array_key_exists('name', $related))
-            {
-                $queryHeader = $queryHeader . ", things t";
-                $name = $related['name'];
-                if ($first)
+                if (array_key_exists('limit', $related))
                 {
-                    $query = $query . "e.id = t.id AND t.name LIKE '%$name%' ";
-                    $first = false;
-                } else $query = $query . "AND e.id = t.id AND t.name LIKE '%$name%' ";
-            }
-            if (array_key_exists('type', $related))
-            {
-                $type = $related['type'];
-                if ($first)
-                {
-                    $query = $query . "e.type LIKE '%$type%' ";
-                    $first = false;
-                } else $query = $query . "AND e.type LIKE '%$type%' ";
-            }
-            if (array_key_exists('startDate', $related))
-            {
-                $startDate = new \DateTime($related['startDate']);
-                $date = $startDate->format('Y-m-d H:i:s');
-                if ($first)
-                {
-                    $query = $query . "e.startDate >= '$date' ";
-                    $first = false;
-                } else $query = $query . "AND e.startDate >= '$date' ";
-                $startDateAdded = true;
-            }
-            if (array_key_exists('endDate', $related))
-            {
-                $endDate = new \DateTime($related['endDate']);
-                $date = $endDate->format('Y-m-d H:i:s');
-                if ($first)
-                {
-                    $query = $query . "e.startDate <= '$date' ";
-                    $first = false;
-                } else $query = $query . "AND e.startDate <= '$date' ";
-            }
-            if (array_key_exists('location', $related))
-            {
-                $queryHeader = $queryHeader . ", things pt, places p, openDataCities o";
-                $city = $related['location'];
-                if ($first)
-                {
-                    $query = $query . "e.location = p.id AND p.id = pt.id AND pt.name = o.name AND o.id = $city ";
-                    $first = false;
-                } else $query = $query . "AND e.location = p.id AND p.id = pt.id AND pt.name = o.name AND o.id = $city ";
-            }
-            if ( ! $startDateAdded)
-            {
-                $now = new \DateTime('now');
-                $date = $now->format('Y-m-d H:i:s');
-                $query = $query . "AND (e.startDate > '$date' or e.startDate IS NULL) ";
-            }
-
-            $query = $queryHeader . $query;
-            //dd($query . "ORDER BY e.startDate ASC LIMIT " . $limit);
-            /*$result = array();
-            $eloquentFilter = array();
-            $manualFilter = array();
-            foreach ($related as $key => $value)
-            {
-                if ($key == 'name' or 'location') $manualFilter[$key] = $value;
-                else $eloquentFilter[$key] = $value;
-            }
-
-            if (empty($eloquentFilter)) $eloquentResult = OpenEvent::all()->paginate(20);
-            else $eloquentResult = OpenEvent::where($eloquentFilter)->get();
-
-            if ( ! empty($eloquentResult) and ! empty($manualFilter))
-            {
-                foreach ($manualFilter as $manualKey => $manualValue)
-                {
-
-
+                    $limit = $related['limit'];
                 }
-            }
-            else $result = $eloquentResult;*/
-            $result = DB::select( DB::raw($query . "ORDER BY e.startDate ASC LIMIT " . $limit) );
-
-            $events = array();
-            if ( ! empty($result))
-            {
-                foreach ($result as $eventInformation)
+                if (array_key_exists('name', $related))
                 {
-                    $events[] = with(new OpenEvent)->newFromStd($eventInformation);
+                    $queryHeader = $queryHeader . ", things t";
+                    $name = $related['name'];
+                    if ($first)
+                    {
+                        $query = $query . "e.id = t.id AND t.name LIKE '%$name%' ";
+                        $first = false;
+                    } else $query = $query . "AND e.id = t.id AND t.name LIKE '%$name%' ";
                 }
+                if (array_key_exists('type', $related))
+                {
+                    $type = $related['type'];
+                    if ($first)
+                    {
+                        $query = $query . "e.type LIKE '%$type%' ";
+                        $first = false;
+                    } else $query = $query . "AND e.type LIKE '%$type%' ";
+                }
+                if (array_key_exists('startDate', $related))
+                {
+                    $startDate = new \DateTime($related['startDate']);
+                    $date = $startDate->format('Y-m-d H:i:s');
+                    if ($first)
+                    {
+                        $query = $query . "e.startDate >= '$date' ";
+                        $first = false;
+                    } else $query = $query . "AND e.startDate >= '$date' ";
+                    $startDateAdded = true;
+                }
+                if (array_key_exists('endDate', $related))
+                {
+                    $endDate = new \DateTime($related['endDate']);
+                    $date = $endDate->format('Y-m-d H:i:s');
+                    if ($first)
+                    {
+                        $query = $query . "e.startDate <= '$date' ";
+                        $first = false;
+                    } else $query = $query . "AND e.startDate <= '$date' ";
+                }
+                if (array_key_exists('location', $related))
+                {
+                    $queryHeader = $queryHeader . ", things pt, places p, openDataCities o";
+                    $city = $related['location'];
+                    if ($first)
+                    {
+                        $query = $query . "e.location = p.id AND p.id = pt.id AND pt.name = o.name AND o.id = $city ";
+                        $first = false;
+                    } else $query = $query . "AND e.location = p.id AND p.id = pt.id AND pt.name = o.name AND o.id = $city ";
+                }
+                if ( ! $startDateAdded)
+                {
+                    $now = new \DateTime('now');
+                    $date = $now->format('Y-m-d H:i:s');
+                    $query = $query . "AND (e.startDate > '$date' or e.startDate IS NULL) ";
+                }
+
+                $query = $queryHeader . $query;
+                //dd($query . "ORDER BY e.startDate ASC LIMIT " . $limit);
+                /*$result = array();
+                $eloquentFilter = array();
+                $manualFilter = array();
+                foreach ($related as $key => $value)
+                {
+                    if ($key == 'name' or 'location') $manualFilter[$key] = $value;
+                    else $eloquentFilter[$key] = $value;
+                }
+
+                if (empty($eloquentFilter)) $eloquentResult = OpenEvent::all()->paginate(20);
+                else $eloquentResult = OpenEvent::where($eloquentFilter)->get();
+
+                if ( ! empty($eloquentResult) and ! empty($manualFilter))
+                {
+                    foreach ($manualFilter as $manualKey => $manualValue)
+                    {
+
+
+                    }
+                }
+                else $result = $eloquentResult;*/
+                $result = DB::select( DB::raw($query . "ORDER BY e.startDate ASC LIMIT " . $limit) );
+
+                $events = array();
+                if ( ! empty($result))
+                {
+                    foreach ($result as $eventInformation)
+                    {
+                        $events[] = with(new OpenEvent)->newFromStd($eventInformation);
+                    }
+                }
+                //dd($result);
+                //dd(DB::getQueryLog());
+                return $events;
             }
-            //dd($result);
-            //dd(DB::getQueryLog());
-            return $events;
-        }
-        else if (array_key_exists('clean', $related))
-        {
-            return OpenEvent::all();
+            else if (array_key_exists('clean', $related))
+            {
+                return OpenEvent::all();
+            }
+            else
+            {
+                return OpenEvent::take(50)->get();
+            }
         }
         else
         {
